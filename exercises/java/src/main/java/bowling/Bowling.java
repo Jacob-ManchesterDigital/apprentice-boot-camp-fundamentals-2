918e18f864f1c608563919f5a4afed131d3e4d34
Jacob Walsh <walsh+manchesterdigital@posteo.net>
2021-04-19 11:46:47 +0100

    Add gutter game test and integrate with Bowling

---
+++ exercises/java/src/main/java/bowling/Bowling.java
@@ -0,0 +1,7 @@
+package bowling;
+
+public class Bowling {
+    public static int calculateScoreFromRolls(int[] rolls) {
+        return 0;
+    }
+}
--- exercises/java/src/test/java/bowling/BowlingTest.java
+++ exercises/java/src/test/java/bowling/BowlingTest.java
@@ -1,10 +1,20 @@
 package bowling;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class BowlingTest {
     @Test
-    public void renameMe() {
-        
+    public void whenAllZerosScoredThenReturnZero() {
+        // Arrange
+        int expectedScore = 0;
+        int [] rolls = {0,0, 0,0, 0,0, 0,0, 0,0,
+                0,0, 0,0, 0,0, 0,0, 0,0};
+
+        // Act
+        int outputScore = Bowling.calculateScoreFromRolls(rolls);
+
+        // Assert
+        Assertions.assertThat(outputScore).isEqualTo(expectedScore);
     }
 }
