be4625f1de3ec4a83f83f71d6cb59cfe2be51f09
Jacob Walsh <walsh+manchesterdigital@posteo.net>
2021-04-19 15:49:11 +0100

    Refactor calculateScoreFromRolls by splitting strike and spare checks into isStrike and isSpare

--- exercises/java/src/main/java/bowling/Bowling.java
+++ exercises/java/src/main/java/bowling/Bowling.java
@@ -3,19 +3,20 @@ package bowling;
 public class Bowling {
     public int calculateScoreFromRolls(int[] rolls) {
         int score = 0;
-
         int rollIndex = 0;
+
         for (int frame = 0; frame < 10; frame++) {
 
-            if (rolls[rollIndex] == 10){
+            // if strike
+            if (isStrike(rollIndex,rolls)){
                 score +=  rolls[rollIndex] + rolls[rollIndex + 1] + rolls[rollIndex + 2];
                 rollIndex += 1;
             }
-            else if (rolls[rollIndex] + rolls[rollIndex + 1] == 10){
-                score +=  rolls[rollIndex] + rolls[rollIndex + 1];
-                score += rolls[rollIndex + 2];
+            // if spare
+            else if (isSpare(rollIndex,rolls)){
+                score +=  rolls[rollIndex] + rolls[rollIndex + 1] + rolls[rollIndex + 2];
                 rollIndex += 2;
-            }
+            } // not strike or spare
             else{
                 score +=  rolls[rollIndex] + rolls[rollIndex + 1];
                 rollIndex += 2;
@@ -23,5 +24,13 @@ public class Bowling {
         }
 
         return score;
+    }
+
+    private boolean isStrike(int rollIndex, int[] rolls){
+        return rolls[rollIndex] == 10;
+    }
+
+    private boolean isSpare(int rollIndex, int[] rolls){
+        return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
     }
 }
